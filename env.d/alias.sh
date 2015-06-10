68ca04492f0544f1b803835e1c1a2ae031f004fc
Quido Meijer <quidome@gmail.com>
2015-06-10 22:49:24 +0200

    Add rbenv to env.d

--- env.d/alias.sh
+++ env.d/alias.sh
@@ -3,3 +3,7 @@ alias ll='ls -la'
 
 # Sublime
 alias s=subl
+
+# Puppet
+alias cdhiera='cd /Users/qmeijer/development/bol.com/puppet_vagrant/hiera'
+alias cdmodules='cd /Users/qmeijer/development/bol.com/puppet_vagrant/modules'
