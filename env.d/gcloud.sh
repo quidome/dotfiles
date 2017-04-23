5f6f6453c2f957cb8a880162ed62baa7a8508020
Quido Meijer <quidome@gmail.com>
2017-04-23 11:03:16 +0200

    fix typo

--- env.d/gcloud.sh
+++ env.d/gcloud.sh
@@ -2,4 +2,4 @@
 if [ -f "${HOME}/Applications/google-cloud-sdk/path.zsh.inc" ]; then source "${HOME}/Applications/google-cloud-sdk/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc" ]; then source "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc" ]; then source "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc"; fi
