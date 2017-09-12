c7b542acaac55393851e8a17198fb67c6ec77f7c
Quido Meijer <quidome@gmail.com>
2017-09-12 22:55:25 +0200

    Add some helper functions to python virtualenv

--- env.d/python-env.sh
+++ env.d/python-env.sh
@@ -6,3 +6,12 @@ source /usr/local/bin/virtualenvwrapper.sh
 if (tty -s); then
   workon py3
 fi
+
+# gpip function allows to pip install in global config
+gpip2(){
+  PIP_REQUIRE_VIRTUALENV="" pip2 "$@"
+}
+
+gpip3(){
+  PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
+}
