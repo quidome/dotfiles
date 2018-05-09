847c940972ac3b2d131a7fb9b9d9b220a4c65aef
Quido Meijer <quidome@gmail.com>
2018-05-09 22:33:21 +0200

    changes invoke of ara in ansible

--- env.d/ansible.sh
+++ env.d/ansible.sh
@@ -7,10 +7,14 @@ ANSIBLE_RETRY_FILES_ENABLED=no
 function anson(){
     # enable ansible
     workon ansible
-    export ANSIBLE_CALLBACK_PLUGINS="$(python -c 'import os,ara; print(os.path.dirname(ara.__file__))')/plugins/callbacks"
+    # export ANSIBLE_CALLBACK_PLUGINS="$(python -c 'import os,ara; print(os.path.dirname(ara.__file__))')/plugins/callbacks"
+    source <(python -m ara.setup.env)
 }
 
 function ansoff() {
     unset ANSIBLE_CALLBACK_PLUGINS
+    unset ANSIBLE_ACTION_PLUGINS
+    unset ANSIBLE_LIBRARY
+    unset PYTHONPATH
     deactivate
 }
