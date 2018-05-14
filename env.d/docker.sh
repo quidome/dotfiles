133ff347df36d70157a806b4e421c156bab2d9a6
Quido Meijer <quidome@gmail.com>
2018-05-14 10:48:36 +0200

    adds docker and java

---
+++ env.d/docker.sh
@@ -0,0 +1,47 @@
+function fedora()
+{
+    local container_name=my_fedora
+    local image_name='fedora'
+    func_rundocker "${container_name}" "${image_name}"
+}
+
+
+function debian()
+{
+    local container_name=my_debian
+    local image_name=debian
+    func_rundocker "${container_name}" "${image_name}"
+}
+
+
+function arch() {
+    local container_name=my_arch
+    local image_name='archlinux/base:latest'
+
+    func_rundocker "${container_name}" "${image_name}"
+}
+
+
+function func_rundocker() {
+    # check arguments
+    if [ $# -ne 2 ]; then
+	echo "Arguments mismatch: need 2, got ${#}"
+    else
+	local docker_status container_name image_name
+	container_name=${1}
+	image_name=${2}
+
+	docker_status=$(docker ps -a -f name="${container_name}$" --format {{.Status}})
+
+	if [[ -z "$docker_status" ]]; then
+	    echo "no traces of ${container_name} found, run a new one"
+	    docker run --name ${container_name} -h ${container_name}.local -i -t ${image_name} /bin/bash
+	elif [[ $docker_status =~ ^Up ]]; then
+	    echo "attaching to running ${container_name} instance: ${docker_status}"
+	    docker attach ${container_name}
+	else
+	    echo "restarting stopped ${container_name} instance: ${docker_status}"
+	    docker start -a -i ${container_name}
+	fi
+    fi
+}
---
+++ tag-osx/env.d/java.sh
@@ -0,0 +1,5 @@
+
+
+alias j9="export JAVA_HOME=`/usr/libexec/java_home -v 9`; java -version"
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+#alias j7="export JAVA_HOME=`/usr/libexec/java_home -v 1.7`; java -version"
