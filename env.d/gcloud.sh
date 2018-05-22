1efaf413aae13e9ea2601a2486967b361a5b2111
Quido Meijer <quidome@gmail.com>
2018-05-22 18:15:49 +0200

    Adds materialized colors to termite
    and cleans up i3/config
    and adds some kubectx aliases

--- env.d/gcloud.sh
+++ env.d/gcloud.sh
@@ -3,3 +3,7 @@ if [ -f "${HOME}/Applications/google-cloud-sdk/path.zsh.inc" ]; then source "${H
 
 # The next line enables shell command completion for gcloud.
 if [ -f "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc" ]; then source "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc"; fi
+
+# set aliases for kubectx and kubens
+alias kns=kubens
+alias kctx=kubectx
