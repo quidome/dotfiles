12d5578b4846b354f773e883d7bcb43e506630c6
Quido Meijer <quidome@gmail.com>
2018-05-28 17:39:17 +0200

    Cleans up some linux gpg stuff
    and adds working keybindings for taking screenshots

--- env.d/gnupg.sh
+++ env.d/gnupg.sh
@@ -19,15 +19,15 @@ if [[ $OSTYPE == darwin* ]]; then
   export GPG_TTY
 elif [[ $OSTYPE == linux* ]]; then
   # Start the gpg-agent if not already running
-  if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
-    gpg-connect-agent /bye >/dev/null 2>&1
-  fi
+  #if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+  #  gpg-connect-agent /bye >/dev/null 2>&1
+  #fi
 
   # Set SSH to use gpg-agent
-  unset SSH_AGENT_PID
-  if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-  fi
+  #unset SSH_AGENT_PID
+  #if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  #  export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+  #fi
 
   # Set GPG TTY
   GPG_TTY=$(tty)
