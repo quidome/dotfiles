1f13e64bebe1334ce4f41e628e1bc40feafe8b4c
Quido Meijer <quidome@gmail.com>
2018-07-30 13:58:26 +0200

    Clean up of coloring stuff and gcloud usage

--- env.d/gcloud.sh
+++ env.d/gcloud.sh
@@ -1,8 +1,8 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f "${HOME}/Applications/google-cloud-sdk/path.zsh.inc" ]; then source "${HOME}/Applications/google-cloud-sdk/path.zsh.inc"; fi
+if [ -f "${HOME}/opt/google-cloud-sdk/path.zsh.inc" ]; then source "${HOME}/opt/google-cloud-sdk/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc" ]; then source "${HOME}/Applications/google-cloud-sdk/completion.zsh.inc"; fi
+if [ -f "${HOME}/opt/google-cloud-sdk/completion.zsh.inc" ]; then source "${HOME}/opt/google-cloud-sdk/completion.zsh.inc"; fi
 
 # set aliases for kubectx and kubens
 alias kns=kubens
--- zshrc
+++ zshrc
@@ -110,9 +110,3 @@ fi
 zstyle -s ':completion:*:hosts' hosts _ssh_config
 [[ -r ${PUPPET_HOSTS} ]] && _ssh_config+=($(cat ${PUPPET_HOSTS}))
 zstyle ':completion:*:hosts' hosts $_ssh_config
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/qmeijer/Downloads/google-cloud-sdk/path.zsh.inc' ]; then source '/home/qmeijer/Downloads/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/home/qmeijer/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then source '/home/qmeijer/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
