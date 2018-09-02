e89a202acd03416002e6edef5f50a0e5e061ffc8
Quido Meijer <quidome@gmail.com>
2018-09-02 14:55:18 +0200

    Minor updates to linux config

--- env.d/gnupg.sh
+++ env.d/gnupg.sh
@@ -30,9 +30,9 @@ elif [[ $OSTYPE == linux* ]]; then
   #fi
 
   # Set GPG TTY
-  GPG_TTY=$(tty)
-  export GPG_TTY
+  #GPG_TTY=$(tty)
+  #export GPG_TTY
 
   # Refresh gpg-agent tty in case user switches into an X session
-  gpg-connect-agent updatestartuptty /bye >/dev/null
+  #gpg-connect-agent updatestartuptty /bye >/dev/null
 fi
