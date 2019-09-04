b891b1d09455cf68bfb68122b7bb88b1c3ca2393
Quido Meijer <quidome@gmail.com>
2019-09-05 00:49:02 +0200

    Fix script indentation

--- env.d/50_git-projects.sh
+++ env.d/50_git-projects.sh
@@ -7,35 +7,35 @@ alias gpr=func_gitproject
 alias opr=func_openproject
 
 func_gitproject() {
-  if [ $# -eq 0 ]; then
-    echo "## gpr projects in ${GITPROJECT}:"
-    ls -1 "${GITPROJECT}"
-  else
+    if [ $# -eq 0 ]; then
+	echo "## gpr projects in ${GITPROJECT}:"
+	ls -1 "${GITPROJECT}"
+    else
+	project=$1
+
+	# perform git action on project
+	if [ -e "${GITPROJECT}/${project}" ] ; then
+	    while IFS= read -r repo
+	    do
+		echo "## ${repo}"
+		git -C "${repo}" "${@:2}"
+	    done < "${GITPROJECT}/${project}"
+	else
+	    echo "${GITPROJECT}/${1} does not exist"
+	fi
+    fi
+}
+
+func_openproject() {
     project=$1
 
     # perform git action on project
     if [ -e "${GITPROJECT}/${project}" ] ; then
-      while IFS= read -r repo
-      do
-	      echo "## ${repo}"
-	      git -C "${repo}" "${@:2}"
-      done < "${GITPROJECT}/${project}"
+	while IFS= read -r repo
+	do
+	    atom -a "${repo}"
+	done < "${GITPROJECT}/${project}"
     else
-      echo "${GITPROJECT}/${1} does not exist"
+	echo "${GITPROJECT}/${1} does not exist"
     fi
-  fi
-}
-
-func_openproject() {
-  project=$1
-
-  # perform git action on project
-  if [ -e "${GITPROJECT}/${project}" ] ; then
-    while IFS= read -r repo
-    do
-      atom -a "${repo}"
-    done < "${GITPROJECT}/${project}"
-  else
-    echo "${GITPROJECT}/${1} does not exist"
-  fi
 }
