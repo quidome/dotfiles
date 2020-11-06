3c14e06dbbac05a0c5d62dc7b855b1e4b7cafdf5
Quido Meijer <qmeijer@bol.com>
2020-11-06 18:37:01 +0100

    saving latest state

--- env.d/63_ruby.sh
+++ env.d/63_ruby.sh
@@ -1,22 +1,16 @@
 # shellcheck shell=bash
-if command -v rbenv > /dev/null 2>&1 ; then
 
-    if test -d "${HOME}/.rbenv/bin"; then
-        # include rbenv in path
-        export PATH="$HOME/.rbenv/bin:$PATH"
-    fi
-
-    # init rbenv
+if test -d "${HOME}/.rbenv/bin"; then
+    # include rbenv config if available
+    export PATH="$HOME/.rbenv/bin:$PATH"
     eval "$(rbenv init -)"
 
     # make using gemsets easier
     alias gemset='rbenv gemset'
 
-    # add zsh completion for rbenv
-    . /usr/lib/rbenv/completions/rbenv.zsh
-fi
+elif test -d "$HOME/.rvm/bin/" ; then
+    # otherwise include rvm config if available
 
-if test -d $HOME/.rvm/bin/ ; then
     # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
     export PATH="$PATH:$HOME/.rvm/bin"
 fi
