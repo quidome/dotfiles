d0584d7c375bfd2f70d4797e200b6344dd46f7ac
Quido Meijer <quidome@gmail.com>
2021-02-17 21:28:07 +0100

    Update dotfiles

--- env.d/62_python.sh
+++ env.d/62_python.sh
@@ -6,19 +6,3 @@
 # set place to store virtualenvs
 export WORKON_HOME=~/.virtualenvs
 
-if WRAPPER=$(command -v virtualenvwrapper.sh) ; then
-  # https://github.com/NixOS/nixpkgs/issues/30586
-  # work around a source issue on nixos
-  source <(sed 's/exec/source/' "$WRAPPER")
-else
-  echo "$0: virtualenvwrapper.sh not found in path"
-fi
-
-# gpip function allows to pip install in global config
-gpip2(){
-  PIP_REQUIRE_VIRTUALENV="" pip2 "$@"
-}
-
-gpip3(){
-  PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
-}
