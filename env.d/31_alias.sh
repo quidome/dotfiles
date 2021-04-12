aab0a8bd718dc43f827052db1f43575e696ae6bb
Quido Meijer <quidome@gmail.com>
2021-04-12 19:51:16 +0200

    Adds nixos-option aliases

--- env.d/31_alias.sh
+++ env.d/31_alias.sh
@@ -4,6 +4,9 @@ alias ll='exa -l'
 alias lg='ll --git'
 alias vimdiff='nvim -d'
 
+alias optiondump='nixos-option -r 2>/dev/null > ~/tmp/nixos-option/$(date "+%Y%m%d_%H%M%S")'
+alias optionls='find ~/tmp/nixos-option -type f | sort'
+
 # cssh
 if [[ "$OSTYPE" == "darwin"* ]]; then
     alias cssh='i2cssh -p ${ITERM_PROFILE} -b'
