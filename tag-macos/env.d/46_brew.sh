40c0126782f3ccb81302cdc2382ebbc46345e35e
Quido Meijer <qmeijer@bol.com>
2022-03-13 16:36:04 +0100

    Moves brew setup to macos tag
    And some config has been removed, no longer needed

--- tag-macos/env.d/46_brew.sh
+++ tag-macos/env.d/46_brew.sh
@@ -1,24 +1,6 @@
 # shellcheck shell=bash
-if [[ $OSTYPE == darwin* ]]; then
 
-  # HOMEBREW_GITHUB_API_TOKEN should be set
-  [ ${HOMEBREW_GITHUB_API_TOKEN:+1} ] || echo "HOMEBREW_GITHUB_API_TOKEN is unset"
+export HOMEBREW_BUNDLE_FILE="${HOME}/.config/brew/Brewfile"
 
-  # Add sbin from brew to PATH
-  brew_path=$(brew --prefix)
-  export PATH="$PATH:$brew_path/sbin"
-fi
+alias brew-edit="nvim ${HOMEBREW_BUNDLE_FILE}"
 
-# update and clean brew, including casks
-function brewyouthemother() {
-  BREW_BIN=$(command -v brew)
-
-  $BREW_BIN update
-  $BREW_BIN upgrade
-  $BREW_BIN cask upgrade
-
-  $BREW_BIN cleanup
-
-  $BREW_BIN doctor
-  $BREW_BIN cask doctor
-}
