e2b069edaf5047b4b342f3cfb87de1138aa13331
Quido Meijer <qmeijer@bol.com>
2022-04-12 11:35:48 +0200

    updates gcloud setup

--- tag-macos/env.d/43_gcloud.sh
+++ tag-macos/env.d/43_gcloud.sh
@@ -1,6 +1,10 @@
-# shellcheck shell=bash disable=SC1090
+# shellcheck shell=bash disable=SC1091
 
-source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/qmeijer/Library/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/qmeijer/Library/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/qmeijer/Library/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/qmeijer/Library/google-cloud-sdk/completion.zsh.inc'; fi
 
 # set aliases for kubectx and kubens
 alias kns=kubens
