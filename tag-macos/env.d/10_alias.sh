4b5492bcbcdffae9657040bef7514e6f91bf5477
Quido Meijer <qmeijer@bol.com>
2023-03-10 17:25:46 +0100

    add git clean alias

--- tag-macos/env.d/10_alias.sh
+++ tag-macos/env.d/10_alias.sh
@@ -5,3 +5,5 @@ alias vim=nvim
 alias vi=nvim
 alias vimdiff='nvim -d'
 alias qinit='kinit --password-file=STDIN ${USER}@${KRBDOMAIN} <<<$(gopass show ${PWPATH}/krb/${USER})'
+
+alias gitclean='git branch | grep -vE "\*|main|master" | xargs git branch -d'
