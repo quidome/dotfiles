1353b72c3ccbd8e05d5163bc4444957d0aadb24f
Quido Meijer <qmeijer@bol.com>
2023-04-20 13:40:04 +0200

    remove obsolete aliases

--- tag-macos/env.d/10_alias.sh
+++ tag-macos/env.d/10_alias.sh
@@ -1,9 +1,3 @@
 # shellcheck shell=bash
-alias ll='exa -la'
-
-alias vim=nvim
-alias vi=nvim
-alias vimdiff='nvim -d'
 alias qinit='kinit --password-file=STDIN ${USER}@${KRBDOMAIN} <<<$(gopass show ${PWPATH}/krb/${USER})'
-
 alias gitclean='git branch | grep -vE "\*|main|master" | xargs git branch -d'
