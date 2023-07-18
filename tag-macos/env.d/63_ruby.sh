ebc9b1d3640f22a7db3254f727b9861d7d8168e4
Quido Meijer <qmeijer@bol.com>
2023-07-18 09:09:55 +0200

    Minor ruby env changes

--- tag-macos/env.d/63_ruby.sh
+++ tag-macos/env.d/63_ruby.sh
@@ -1,16 +1,14 @@
 # shellcheck shell=bash
 
-if test -d "${HOME}/.rbenv/bin"; then
+if test -f "${HOME}/.rbenv/bin/rbenv"; then
     # include rbenv config if available
-    export PATH="$HOME/.rbenv/bin:$PATH"
-    eval "$(rbenv init -)"
+    eval "$(~/.rbenv/bin/rbenv init - zsh)"
 
     # make using gemsets easier
     alias gemset='rbenv gemset'
 
 elif test -d "$HOME/.rvm/bin/" ; then
     # otherwise include rvm config if available
-
     # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
     export PATH="$PATH:$HOME/.rvm/bin"
 fi
