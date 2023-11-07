edf0a24270aa9855bd8d8e83c1daaee596f116b1
Quido Meijer <qmeijer@bol.com>
2023-11-07 14:13:45 +0100

    Remove preloading
    just load at regular spot

--- tag-macos/env.d/43_gcloud.sh
+++ tag-macos/env.d/43_gcloud.sh
@@ -3,7 +3,7 @@ if [ -f '/Users/qmeijer/Library/google-cloud-sdk/path.zsh.inc' ]; then . '/Users
 
 # The next line enables shell command completion for gcloud.
 # This line was moved to the slow stuff preloader
-#if [ -f '/Users/qmeijer/Library/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/qmeijer/Library/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '/Users/qmeijer/Library/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/qmeijer/Library/google-cloud-sdk/completion.zsh.inc'; fi
 
 # set aliases for kubectx and kubens
 alias k=kubectl
