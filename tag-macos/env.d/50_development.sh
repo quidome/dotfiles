a765e0db038df5695726540489f7c98e0f529e59
Quido Meijer <quidome@gmail.com>
2024-01-11 09:22:55 +0100

    Clean up this file, less tooling
    stick with
    - pyenv/virtualenv for python
    - regular golang install
    - fnm manager for nodejs
    - rbenv manager for ruby

--- tag-macos/env.d/50_development.sh
+++ tag-macos/env.d/50_development.sh
@@ -1,40 +1,24 @@
 # shellcheck shell=bash
 
-# Add golang install dir to path
-export PATH=$PATH:${HOME}/go/bin
-
 # Add Intellij to path
 PATH=$PATH:'/Applications/IntelliJ IDEA.app/Contents/MacOS'
 
-# Manage python with pyenv
+# Golang
+export PATH=$PATH:${HOME}/go/bin
+
+# Python
 if command -v pyenv 1> /dev/null 2>&1 ; then
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
 fi
 
-# Manage ruby with either rbenv or rvm
+# Ruby
 if test -f "${HOME}/.rbenv/bin/rbenv"; then
-    # include rbenv config if available
-    eval "$(~/.rbenv/bin/rbenv init - zsh)"
-
-    # make using gemsets easier
-    alias gemset='rbenv gemset'
-
-elif test -d "$HOME/.rvm/bin/" ; then
-    # otherwise include rvm config if available
-    # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-    export PATH="$PATH:$HOME/.rvm/bin"
+  eval "$(~/.rbenv/bin/rbenv init - zsh)"
+  alias gemset='rbenv gemset'
 fi
 
-# Multi language manager rtx (asdf clone)
-# used for: nodejs
-if command -v rtx 1> /dev/null 2>&1 ; then
-    eval "$(rtx activate zsh)"
-fi
-
-# Manage languages with sdkman
-# used for: java
-export SDKMAN_DIR="$SDKMAN_DIR_RAW"
-if test -f "${HOME}/.sdkman/bin/sdkman-init.sh"; then
-    source "${HOME}/.sdkman/bin/sdkman-init.sh"
+# Nodejs
+if command -v fnm >/dev/null ; then
+  eval "$(fnm env --use-on-cd)"
 fi
