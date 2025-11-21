3a109575bfb369345e716629437c332d60532cfe
Quido Meijer <quidome@gmail.com>
2025-11-21 17:41:17 +0100

    fix aliases

--- .env.d/61-cloud.sh
+++ .env.d/61-cloud.sh
@@ -2,6 +2,6 @@
 . "/opt/homebrew/share/google-cloud-sdk/path.zsh.inc"
 . "/opt/homebrew/share/google-cloud-sdk/completion.zsh.inc"
 
-alias k =kubectl
-alias kn =kubens
-alias kc =kubectx
+alias k=kubectl
+alias kn=kubens
+alias kc=kubectx
