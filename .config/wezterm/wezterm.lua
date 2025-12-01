065e45d13386c5d5556d6b8b4ac75797d589e856
Quido Meijer <quidome@gmail.com>
2025-12-01 10:19:37 +0100

    Change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,20 @@
 -- See https://wezfurlong.org/wezterm/
-
 local wezterm = require 'wezterm'
 
 return {
-  enable_tab_bar = false,
-  font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Light'}),
-  font_size = 13,
-  color_scheme = "Monokai Remastered",
+    enable_tab_bar = false,
+    font = wezterm.font('JetBrainsMono Nerd Font'),
+    font_size = 13,
+    color_scheme = "Monokai Remastered",
 
-  initial_cols = 120,
-  initial_rows = 40,
+    initial_cols = 120,
+    initial_rows = 40,
 
-  -- Window
-  window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
-  },
+    -- Window
+    window_padding = {
+        left = 10,
+        right = 10,
+        top = 10,
+        bottom = 10,
+    },
 }
