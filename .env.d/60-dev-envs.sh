28ec57d90799c8d10881960b587a0b43ccb3f3f9
Quido Meijer <quidome@gmail.com>
2025-12-02 14:20:54 +0100

    Fix gemini setup

--- .env.d/60-dev-envs.sh
+++ .env.d/60-dev-envs.sh
@@ -26,6 +26,3 @@ PATH=$PATH:$HOME/go/bin
 PATH=$PATH:$HOME/.cargo/bin
 PATH=$PATH:"/Applications/IntelliJ IDEA.app/Contents/MacOS"
 export PATH
-
-# Set gcloud project for gemini
-export GOOGLE_CLOUD_PROJECT=bolcom-pro-gemini-poc
