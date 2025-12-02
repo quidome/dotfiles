438137744ede7e067f39bd846b4bd769888a421d
Quido Meijer <quidome@gmail.com>
2025-12-02 14:20:18 +0100

    make psql available

--- .env.d/50-middle.sh
+++ .env.d/50-middle.sh
@@ -26,3 +26,6 @@ unset SSH_AUTH_SOCK
 export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+
+# Make psql available
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
